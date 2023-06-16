a50d6f542c16e91ef85328abf0075d854dc34875
veprogames <75524847+veprogames@users.noreply.github.com>
2023-06-16 21:12:15 +0200

    make bullet visible more easily

--- bullet/bullet.gd
+++ bullet/bullet.gd
@@ -6,6 +6,7 @@ extends Area2D
 
 func _physics_process(delta: float) -> void:
 	position += Vector2.RIGHT.rotated(direction) * speed * delta
+	rotation = direction + PI / 2
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
