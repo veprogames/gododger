2e4f81a742f537f92f633140e5bc0e7c5cc1ca5f
veprogames <75524847+veprogames@users.noreply.github.com>
2023-06-16 10:53:23 +0200

    improve background particles

--- musicsyncer/music_syncer.gd
+++ musicsyncer/music_syncer.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var environment: WorldEnvironment
 @export var camera: Camera2D
+@export var bg_particles: GPUParticles2D
 var analyzer := AudioServer.get_bus_effect_instance(0, 0) as AudioEffectSpectrumAnalyzerInstance
 
 func _ready() -> void:
@@ -14,3 +15,4 @@ func _process(_delta: float) -> void:
 		var intensity := analyzer.get_magnitude_for_frequency_range(20, 20000).length()
 		environment.environment.glow_strength = 1.1 + intensity * 0.7
 		camera.offset = Vector2(randf_range(-1, 1), randf_range(-1, 1)) * intensity
+		bg_particles.speed_scale = 24 * intensity
