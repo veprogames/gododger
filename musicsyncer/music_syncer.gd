9d7337c0c255d4aee0bcf7f8ccdeebc761709e2a
veprogames <75524847+veprogames@users.noreply.github.com>
2023-07-04 17:03:18 +0200

    improve visibility of objects

--- musicsyncer/music_syncer.gd
+++ musicsyncer/music_syncer.gd
@@ -17,6 +17,6 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if analyzer:
 		var intensity := analyzer.get_magnitude_for_frequency_range(20, 20000).length()
-		environment.environment.glow_strength = 1.1 + intensity * 0.7 * glow_strength
+		environment.environment.glow_strength = 1.1 + intensity * 0.6 * glow_strength
 		camera.offset = Vector2(randf_range(-1, 1), randf_range(-1, 1)) * intensity * shake_strength
 		bg_particles.speed_scale = 24 * intensity
